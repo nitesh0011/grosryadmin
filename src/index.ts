c3ad7e0a6cb45d51e623b95663e5aeb659a0d605
nitesh0011 <patilnitesh590@gmail.com>
2024-06-21 20:27:25 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export default {
+  /**
+   * An asynchronous register function that runs behtmlFore
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register(/*{ strapi }*/) {},
+
+  /**
+   * An asynchronous bootstrap function that runs behtmlFore
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perhtmlForm some special logic.
+   */
+  bootstrap(/*{ strapi }*/) {},
+};
