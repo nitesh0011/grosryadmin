8b82f7ac22d6988dbe8df60367f07eb83ed84b44
nitesh0011 <patilnitesh590@gmail.com>
2024-06-22 20:11:00 +0530

    hello new change

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,24 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  
+];
+
+module.exports = [
+  // ...
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // ...
 ];
